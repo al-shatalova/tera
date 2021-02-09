93ea073bf007163bce8f904897670f3a85dd2824
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2021-02-09 13:03:35 +0300

    Сделал лабу
    ВСЕ СЧДЕЛАЛ

---
+++ like_button.js
@@ -0,0 +1,26 @@
+'use strict'
+const e = React.createElement
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked comment number " + this.props.commentID;
+    }
+    return e(
+      "button",
+      { onClick: () => this.setState({ liked: true }) },
+      "Like"
+    )
+  }
+}
+
+const domContainer = document.querySelectorAll('.likebut')
+.forEach(domContainer=>{
+    const commentID = parseInt(domContainer.dataset.commentid,10)
+    ReactDOM.render(e(LikeButton, {commentID:commentID}),
+    domContainer)
+})
