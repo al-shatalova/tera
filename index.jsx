a2351af57664564e40eadbe43b350715e5840f69
Andrey Baranyuk <34580676+AndreyBaranyuk@users.noreply.github.com>
2021-03-23 13:01:42 +0300

    lab23.03

--- index.jsx
+++ index.jsx
@@ -1,92 +1,64 @@
-class ClickButton extends React.Component {
-  constructor(props) {
+class UserForm extends React.Component {
+    constructor(props) {
       super(props);
-      this.state = {class: "off", label: "Нажми"};
-        
-      this.press = this.press.bind(this);
-        
-      console.log("constructor");
+      var name = props.name;
+      var nameIsValid = this.validateName(name);
+      var age = props.age;
+      var ageIsValid = this.validateAge(age);
+      this.state = {name: name, age: age, nameValid: nameIsValid, ageValid: ageIsValid};
+ 
+      this.onNameChange = this.onNameChange.bind(this);
+      this.onAgeChange = this.onAgeChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+      validateAge(age){
+          return age>=0;
+      }
+      validateName(name){
+          return name.length>2;
+      }
+      onAgeChange(e) {
+          var val = e.target.value;
+          var valid = this.validateAge(val);
+          this.setState({age: val, ageValid: valid});
+      }
+      onNameChange(e) {
+          var val = e.target.value;
+          console.log(val);
+          var valid = this.validateName(val);
+          this.setState({name: val, nameValid: valid});
+      }
+ 
+      handleSubmit(e) {
+          e.preventDefault();
+          if(this.state.nameValid ===true && this.state.ageValid===true){
+              alert("Имя: " + this.state.name + " Возраст: " + this.state.age);
+          }
+      }
+ 
+      render() {
+          // цвет границы для поля для ввода имени
+          var nameColor = this.state.nameValid===true?"green":"red";
+          // цвет границы для поля для ввода возраста
+          var ageColor = this.state.ageValid===true?"green":"red";
+          return (
+              <form onSubmit={this.handleSubmit}>
+                  <p>
+                      <label>Имя:</label><br />
+                      <input type="text" value={this.state.name} 
+                          onChange={this.onNameChange} style={{borderColor:nameColor}} />
+                  </p>
+                  <p>
+                      <label>Возраст:</label><br />
+                      <input type="number" value={this.state.age} 
+                          onChange={this.onAgeChange}  style={{borderColor:ageColor}} />
+                  </p>
+                  <input type="submit" value="Отправить" />
+              </form>
+          );
+      }
   }
-  componentWillReceiveProps(nextProps) {
-      console.log("componentWillReceiveProps()");
-  }
-};
-  let propsValues = {
-    title: "Список группы",
-    items: [
-        "Сорокин", 
-        "Никитин", 
-        "Баранюк", 
-        "Степанов", 
-        "Имкенова", 
-        "Уральсков"
-    ]
-  };
-              
-class Item extends React.Component {
-    render() {
-        return <li>{this.props.name}</li>;
-    }
-}
-         
-class SearchPlugin extends React.Component{
-             
-    constructor(props){
-        super(props);
-        this.onTextChanged = this.onTextChanged.bind(this);
-    }
-             
-    onTextChanged(e){
-        var text = e.target.value.trim();   // удаляем пробелы
-        this.props.filter(text); // передаем введенный текст в родительский компонент
-    }
-             
-    render() {
-        return <input placeholder="Поиск" onChange={this.onTextChanged} />;
-    }
-}
-                  
-class ItemsList extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = { items: this.props.data.items};
-                          
-        this.filterList = this.filterList.bind(this);
-    }
-
-    componentDidMount(){
-      propsValues.items.map(x => console.log(x));
-    }
-    
-    componentDidUpdate(prevProps, prevState, snapshot){
-      console.log("list updated");
-
-    }
-
-    filterList(text){
-        let filteredList = this.props.data.items.filter(function(item){
-            return item.toLowerCase().search(text.toLowerCase())!== -1;
-        }); 
-        this.setState({items: filteredList});
-    }
-              
-    render() {
-        return(
-            <div>         
-                <h2>{this.props.data.title}</h2>
-                <SearchPlugin filter={this.filterList} />
-                <ul>
-                    {
-                        this.state.items.map(function(item){
-                            return <Item key={item} name={item} />
-                        })
-                    }
-                </ul>
-            </div>);
-    }
-}
-          
-ReactDOM.render(
-    <ItemsList data={propsValues} />,
-    document.getElementById("app")
-)
+  ReactDOM.render(
+      <UserForm name="" age="0" />,
+      document.getElementById("app")
+  )
