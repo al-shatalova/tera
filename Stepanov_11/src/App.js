f0c40a0685932232e82165d6f2e9c0b3241e4cc7
StepIgor <stepanow.igor@gmail.com>
2021-04-14 16:36:57 +0300

    Stepanov_11

---
+++ Stepanov_11/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="main_header">Лабораторная №11</h1>
+      <h2 className="description">Разбираем типичную структуру приложения</h2>
+    </div>
+  );
+}
