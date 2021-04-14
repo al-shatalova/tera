ea209924f695c4b7b7a4c13b27a24ad0d08a2065
Gorbunov-PI18-3 <55445249+Gorbunov-PI18-3@users.noreply.github.com>
2021-04-14 14:57:05 +0300

    Add files via upload

---
+++ Lab11/src/App1.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App1() {
+  return (
+    <div className="App1">
+      <h1>Лучшие авиалинии для комфортного путешествия эконом-классом.</h1>
+    </div>
+  );
+}
---
+++ Lab11/src/index.js
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App1 from "./App1";
+import App2 from "./App2";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App1 />
+    <App2 />
+  </StrictMode>,
+  rootElement
+);
