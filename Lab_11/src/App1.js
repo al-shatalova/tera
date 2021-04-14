91b9d1cd8afd05977dda81284152d688a6353fe5
grenadinio <56429768+grenadinio@users.noreply.github.com>
2021-04-14 16:43:01 +0300

    Maximov Lab-11

---
+++ Lab_11/src/App1.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App1">
+      <h1>Лучшие авиалинии для комфортного
+        путешествия эконом-классом</h1>
+    </div>
+  );
+}
