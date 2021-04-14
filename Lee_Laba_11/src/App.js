9ed5accbe521ed274ee628b1f727d70eda0cc385
Kirill <kirilllee1998@gmail.com>
2021-04-14 16:40:45 +0300

    Lee_11

---
+++ Lee_Laba_11/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Сказка о попе и работнике его Балде</h1>
+
+    </div>
+  );
+}
---
+++ Lee_Laba_11/src/App2.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App2() {
+  return (
+    <div className="App2">
+    <a href="https://azku.ru/pushkin-skazki/skazka-o-pope-i-rabotnike-ego-balde.html">Ссылка на полную сказку</a>
+    </div>
+  );
+}
