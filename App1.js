f884f2bc0382e483a4341fbc637ff1a3cadef6a7
10Alex71 <55087506+10Alex71@users.noreply.github.com>
2021-04-14 14:58:34 +0300

    Лаба11

---
+++ App1.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App1() {
+  return (
+    <div className="App1">
+      <h2>Познакомиться с коллекцией Лувра, не выходя из дома</h2>
+    </div>
+  );
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App1 from "./App1";
+import App2 from "./App2";
+import App3 from "./App3";
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App1 />
+    <App2 />
+    <App3 />
+  </StrictMode>,
+  rootElement
+);
