026196114328b0928c84b7950b1cdf6b0b7a7ad1
belenkayas <55450454+belenkayas@users.noreply.github.com>
2021-04-15 13:36:33 +0300

    Беленькая лаб11

---
+++ App1.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App1() {
+  return (
+    <div className="App1">
+      <h1>Пять фильмов, которым более 20 лет, но в это сложно поверить</h1>
+    </div>
+  );
+}
