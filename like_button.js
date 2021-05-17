5e771030e3cc07951c1a20ace90627209df3004f
RoyalGoose <velesn@ya.ru>
2021-05-17 14:46:18 +0300

    lab1

--- like_button.js
+++ like_button.js
@@ -1,49 +1,32 @@
 'use strict';
 
-
-
 const e = React.createElement;
 
-
-
 class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    };
 
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+    render() {
+        if (this.state.liked) {
+            //return 'You liked this.';
+            return 'You liked comment number ' + this.props.commentID;
+        }
 
-
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked comment number ' + this.props.commentID;
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
     }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-
-  }
-
 }
 
-
-
-
 document.querySelectorAll('.like_button_container')
-
-  .forEach(domContainer => {
-
-    const commentID = parseInt(domContainer.dataset.commentid, 10);
-
+.forEach(domContainer => {
+    const comid = parseInt(domContainer.dataset.commentid, 10);
     ReactDOM.render(
-
-      e(LikeButton, { commentID: commentID }),
-
-      domContainer
-
+        e(LikeButton, { commentID: comid}),
+        domContainer
     );
-  });
+});
