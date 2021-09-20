a82f4d5956685a5796af8bc11c8a2a3888487741
nect88 <55210686+nect88@users.noreply.github.com>
2021-09-20 14:48:54 +0300

    Komissarov_1

---
+++ Komissarov_1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "pink",  justifyContent: "center", alignItems: "center" }}>
+      <Text>
+        I Try Love React &#128068;
+      </Text>
+    </View>
+  );
+}
+
+export default YourApp;
