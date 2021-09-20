164d76ff9cbd708ccc42e64f2c9f5ee66901fb5d
Tiffany Black (Farbovskaya) <55450516+TiffanyB1ack@users.noreply.github.com>
2021-09-20 14:46:54 +0300

    Farbovskaya_1

---
+++ Farbovskaya_1.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+
+
+const YourApp =() =>{
+  return(
+  <View style={{flex: 3, justifyContent: "center",alignItems:"center"}}>
+      <Text>
+      Hello World!
+      </Text>
+   
+  </View>
+  );
+}
+export default YourApp;
