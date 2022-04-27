80e71c36de9c6ebb7c7d316f3f7859ee019d55fe
Georgiy <valyaev2002@bk.ru>
2022-04-27 13:11:49 +0300

    React App Structure

---
+++ Framework_React/App_Structure/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header.js";
+import Particle from "./Particles.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Particle />
+    </div>
+  );
+}
+
+export default App;
---
+++ Framework_React/App_Structure/src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import logo from './profile.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h1 className="title">Valyaev Georgiy</h1>
+        <h2>Russian IT-student</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
